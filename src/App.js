4c0c617dba9d6cc651a5754a0e246ae1e35cde9b
daniyalImran007 <daniyal_imran@live.com>
2025-11-05 21:10:47 +0500

    changes to deploy for testing of main

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, CI/CD World! Testing main branch</h1>
+      <h1>Hello, CI/CD World! Testing main branch1</h1>
     </div>
   );
 }
